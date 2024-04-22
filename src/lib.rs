355982e14a911cc82ae4b562f6605859fb2eef6c
A4-Tacks <wdsjxhno1001@163.com>
2024-04-22 15:13:24 +0800

    Added some convenient type convert traits

--- src/lib.rs
+++ src/lib.rs
@@ -9,6 +9,7 @@ mod serialize;
 pub use serialize::SYMLSerialize;
 
 use linked_hash_map::LinkedHashMap;
+use core::{char, num, str};
 
 pub type Table = LinkedHashMap<String, Value>;
 
@@ -60,7 +61,23 @@ impl Value {
         }
     }
 
+    pub fn as_string_mut(&mut self) -> Option<&mut String> {
+        if let Self::String(v) = self {
+            Some(v)
+        } else {
+            None
+        }
+    }
+
     pub fn as_array(&self) -> Option<&Vec<Self>> {
+        if let Self::Array(v) = self {
+            Some(v)
+        } else {
+            None
+        }
+    }
+
+    pub fn as_array_mut(&mut self) -> Option<&mut Vec<Self>> {
         if let Self::Array(v) = self {
             Some(v)
         } else {
@@ -75,6 +92,33 @@ impl Value {
             None
         }
     }
+
+    pub fn as_table_mut(&mut self) -> Option<&mut Table> {
+        if let Self::Table(v) = self {
+            Some(v)
+        } else {
+            None
+        }
+    }
+
+    pub fn as_str(&self) -> Option<&str> {
+        Some(&**self.as_string()?)
+    }
+
+    /// use [`as_array`]
+    ///
+    /// [`as_array`]: Self::as_array
+    pub fn as_slice(&self) -> Option<&[Self]> {
+        Some(&**self.as_array()?)
+    }
+
+    /// use [`as_array_mut`]
+    ///
+    /// [`as_array_mut`]: Self::as_array_mut
+    pub fn as_slice_mut(&mut self) -> Option<&mut [Self]> {
+        Some(&mut **self.as_array_mut()?)
+    }
+
 }
 impl From<&'_ str> for Value {
     fn from(value: &'_ str) -> Self {
@@ -173,4 +217,114 @@ impl TryInto<Table> for Value {
             oth => Err(oth),
         }
     }
+}
+impl<'a> TryInto<&'a str> for &'a Value {
+    type Error = ();
+
+    fn try_into(self) -> Result<&'a str, Self::Error> {
+        self.as_str().ok_or(())
+    }
+}
+impl<'a> TryInto<&'a String> for &'a Value {
+    type Error = ();
+
+    fn try_into(self) -> Result<&'a String, Self::Error> {
+        self.as_string().ok_or(())
+    }
+}
+impl<'a> TryInto<&'a [Value]> for &'a Value {
+    type Error = ();
+
+    fn try_into(self) -> Result<&'a [Value], Self::Error> {
+        self.as_slice().ok_or(())
+    }
+}
+impl<'a> TryInto<&'a Vec<Value>> for &'a Value {
+    type Error = ();
+
+    fn try_into(self) -> Result<&'a Vec<Value>, Self::Error> {
+        self.as_array().ok_or(())
+    }
+}
+impl<'a> TryInto<&'a Table> for &'a Value {
+    type Error = ();
+
+    fn try_into(self) -> Result<&'a Table, Self::Error> {
+        self.as_table().ok_or(())
+    }
+}
+impl<'a> TryInto<&'a mut Table> for &'a mut Value {
+    type Error = Self;
+
+    fn try_into(self) -> Result<&'a mut Table, Self::Error> {
+        if let Value::Table(table) = self {
+            Ok(table)
+        } else {
+            Err(self)
+        }
+    }
+}
+impl<'a> TryInto<&'a mut Vec<Value>> for &'a mut Value {
+    type Error = Self;
+
+    fn try_into(self) -> Result<&'a mut Vec<Value>, Self::Error> {
+        if let Value::Array(arr) = self {
+            Ok(arr)
+        } else {
+            Err(self)
+        }
+    }
+}
+impl<'a> TryInto<&'a mut [Value]> for &'a mut Value {
+    type Error = Self;
+
+    fn try_into(self) -> Result<&'a mut [Value], Self::Error> {
+        if let Value::Array(arr) = self {
+            Ok(&mut arr[..])
+        } else {
+            Err(self)
+        }
+    }
+}
+impl<'a> TryInto<&'a mut String> for &'a mut Value {
+    type Error = Self;
+
+    fn try_into(self) -> Result<&'a mut String, Self::Error> {
+        if let Value::String(str) = self {
+            Ok(str)
+        } else {
+            Err(self)
+        }
+    }
+}
+macro_rules! impl_try_into_parse {
+    (@impl $err:ty = $($num:ty),+) => {
+        $(
+            impl TryInto<$num> for &'_ Value {
+                type Error = Option<$err>;
+
+                /// use [`FromStr`]
+                ///
+                /// [`FromStr`]: core::str::FromStr
+                fn try_into(self) -> Result<$num, Self::Error> {
+                    self.as_str()
+                        .ok_or(None)
+                        .and_then(|s| s.parse()
+                            .map_err(Some))
+                }
+            }
+        )+
+    };
+    ($($err:ty = $($num:ty),+ $(,)?);* $(;)?) => {
+        $(
+            impl_try_into_parse!(@impl $err = $($num),+);
+        )*
+    };
+}
+impl_try_into_parse! {
+    num::ParseIntError      = i8, i16, i32, i64, i128, isize;
+    num::ParseIntError      = u8, u16, u32, u64, u128, usize;
+    num::ParseFloatError    = f32, f64;
+    str::ParseBoolError     = bool;
+    char::ParseCharError    = char;
 }
